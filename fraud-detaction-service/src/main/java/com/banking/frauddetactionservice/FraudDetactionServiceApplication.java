aeab76c944da9b5ca2bde103eb1d09aaa0c5be77
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2026-08-11 17:15:16 +0530

    Initial commit

---
+++ fraud-detaction-service/src/main/java/com/banking/frauddetactionservice/FraudDetactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.frauddetactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetactionServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-detaction-service/src/test/java/com/banking/frauddetactionservice/FraudDetactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.frauddetactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
